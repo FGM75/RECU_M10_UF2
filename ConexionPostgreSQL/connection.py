692471091eb1544d6bf7b14cdb5b93e28d27a012
FGM75 <fgonzalezm75@gmail.com>
2024-06-12 16:31:29 +0200

    fix exercici1, ejercici2

--- ConexionPostgreSQL/connection.py
+++ ConexionPostgreSQL/connection.py
@@ -4,10 +4,10 @@ import psycopg2
 conn = psycopg2.connect(
     database = 'felipeDB',
     user = 'felipe',
-    password = 12234,
+    password = '12234',
     host = 'localhost',
-    port = 3383
+    port = '3383'
 )
-connect.cursor()
+conn.cursor()
 
 print(conn)
