0cb1a5ac04d1635623e2f8554e6b68fb55db0baf
FGM75 <fgonzalezm75@gmail.com>
2024-06-12 16:05:59 +0200

    Ejercicio 1

---
+++ ConexionPostgreSQL/connection.py
@@ -0,0 +1,15 @@
+from sqlite3 import connect
+import psycopg2
+
+
+conn = psycopg2.connect
+database = 'felipeDB',
+user = 'felipe',
+password = 12234,
+host = 'localhost',
+port = 3383
+
+
+connect.cursor
+
+print(conn)
